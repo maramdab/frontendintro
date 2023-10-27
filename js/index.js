043e51ae603ff6f2ae63cc433c7a5024be9c2fef
maramdab <146175281+maramdab@users.noreply.github.com>
2023-10-27 10:56:33 +0300

    index.html was modified

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-alert(hello)
+// alert("hello");
+var name="Maram";
+let age=25;
+const isMarried=true;
+const salary=400;
+const skills=["js","css","html"];
+var person={
+    Name: "Maram",
+    Age: 25,
+    isMarried:true,
+    Salary:400,
+    skills:["HTML","CSS","JS"]
+};
+console.log(typeof(name),"name:",name);
+console.log(typeof(age),"age",age);
+console.log(typeof(isMarried),"isMarried",isMarried);
+console.log(typeof(salary),"salary:",salary);
+console.log(typeof(skills),"skills:",skills)
+console.log(typeof(person),"person:",person);
